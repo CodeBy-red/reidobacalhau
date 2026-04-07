27e657b21d9bbdfa868b41b327f19017f11f20c9
Gabriel <gl451606@gmail.com>
2026-04-07 20:58:48 -0300

    Corrigir servidor para servir arquivos estáticos

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static('.')); // Servir arquivos estáticos
 
 // Configuração Google Sheets
 const credentials = JSON.parse(process.env.GOOGLE_CREDENTIALS);
